1b0063a888c69ed7dd0559585af05a33ee3f5be1
Rose Parker <parker.rose@ymail.com>
2018-08-24 01:21:35 +0000

    Copied Sitora's code and added my function to her object

--- object.js
+++ object.js
@@ -1,4 +1,4 @@
-var guessMyObject = {
+/*var guessMyObject = {
     color: "red",
     shape: "round",
     action: "bounce",
@@ -14,4 +14,38 @@ var guessMyObject = {
 };
 
 console.log(guessMyObject.firstClue());
-console.log(guessMyObject.secondClue());
+console.log(guessMyObject.secondClue()); */
+
+//Sitora's code with my function
+
+var guessWho = {
+   height: ["5 foot, 1 inches"],
+   weight: 196,
+   facialFeature: ["mustache"],
+   clothing: ["blue overalls", "red shirt", "red hat"],
+   profession: ["plumber"],
+   about: function() {
+       alert("The person that you are guessing is " + this.height + " tall and he weighs " + this.weight + " pounds and has a " + this.facialFeature + ".");
+   },
+   job: function() {
+       alert('He loves his job, because he is a ' + this.profession + ' and a hero.');
+   },
+   him: function() {
+       return this.height + 'tall and his weight in pounds is ' + this.weight;
+   }
+};
+alert ("Guess my brother's name! His " + guessWho.facialFeature + " is black and bushy and his favorite outfit involves " + guessWho.clothing[0] + " and a " + guessWho.clothing[1] + "!");
+
+function guessName(name) {
+ if(name == "mario") {
+     alert("You're right!  I'm his brother, Luigi!");
+ } else {
+     alert("Sorry, wrong answer.  Click ok to find out!");
+     guessWho.about();
+     guessWho.job();
+     alert("His name is Mario!  Thanks for playing!")
+    
+ } 
+ 
+}
+
